8110c08afd66c5b407fe61bee555a082b9c4a9e1
John Stevenson <john.stevenson@wsu.edu>
2021-09-07 14:31:34 -0700

    Search term is used to retrieve a list of page titles from Wikipedia API

--- scraping.py
+++ scraping.py
@@ -1,15 +1,26 @@
 import wikipedia as w
 
+SEARCH_TERM_LIMIT = 5
+
 def getSearchTerm():
-    term = input("What would you like to search for?\n")
+    term = input("What would you like to search for? ")
     temp = w.suggest(term)
     if temp != None:
-        term = temp
+        validation = input("Did you mean " + temp + "? (y/n) ")
+        if validation == "y":
+            term = temp
 
     return term
 
+def search(term):
+    return w.search(term, SEARCH_TERM_LIMIT)
+
 def main():
     searchTerm = getSearchTerm()
+    print(searchTerm)
+
+    pages = search(searchTerm)
+    print(pages)
 
 
 main()
