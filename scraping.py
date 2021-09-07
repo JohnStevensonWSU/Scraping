91dc8b24693a23a044d517bc7d21e4fe830dac1f
John Stevenson <john.stevenson@wsu.edu>
2021-09-07 16:05:55 -0700

    Page class stores info about a page and program outputs csv file containing the Pages

--- scraping.py
+++ scraping.py
@@ -1,6 +1,15 @@
 import wikipedia as w
 
 SEARCH_TERM_LIMIT = 5
+OUTPUT_FILE = "output.txt"
+
+class Page:
+  
+    def __init__(self, title, summary, image, url):
+        self.title = title
+        self.summary = summary
+        self.image = image
+        self.url = url
 
 def getSearchTerm():
     term = input("What would you like to search for? ")
@@ -9,27 +18,47 @@ def getSearchTerm():
         validation = input("Did you mean " + temp + "? (y/n) ")
         if validation == "y":
             term = temp
-
     return term
 
-def searchTitles(term):
+def findPages(term):
+    titles = returnTitles(term)
+    pages = returnPages(titles)
+    return pages
+
+def returnTitles(term):
     return w.search(term, SEARCH_TERM_LIMIT)
 
-def searchPages(titles):
+def returnPages(titles):
     pages = []
     for i in titles:
         pages.append(w.page(i))
     return pages
 
+def scanPages(pages):
+    Pages = []
+    for i in pages:
+        Pages.append(Page(i.title, i.summary, i.images[0], i.url))
+    return Pages
+
+def storePages(Pages):
+    write = open(OUTPUT_FILE, 'w')
+    write.write("ID,Title,Summary,Image,URL\n")
+    j = 0
+    for i in Pages:
+        write.write(str(j) + ",\"" + i.title + "\",\"" + i.summary + "\"," + i.image + ",\"" + i.url + "\"\n")
+        j = j + 1
+    write.close()
+
 def main():
     searchTerm = getSearchTerm()
     print(searchTerm)
 
-    pageTitles = searchTitles(searchTerm)
-    print(pageTitles)
-
-    pages = searchPages(pageTitles)
+    pages = findPages(searchTerm)
     print(pages)
-    
+
+    Pages = scanPages(pages)
+    print(Pages)
+
+    storePages(Pages)
 
 main()
